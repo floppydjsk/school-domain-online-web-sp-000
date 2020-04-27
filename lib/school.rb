4eb47f89a7b192d16530de6a211b0793874b418d
Daniel Kim <daniel.kim52@gmail.com>
2020-04-27 05:26:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,19 @@ class School
   end
 
   def add_student(student_name, grade)
-    roster[grade] = []
+    roster[grade] ||= []
     roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
   end
 end
